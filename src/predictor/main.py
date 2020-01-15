c442342220298a8c734e52c15d10ab9ac8b06f75
Ran Rubin <ranrubin@gmail.com>
2020-01-15 14:57:54 +0200

    integration

--- src/predictor/main.py
+++ src/predictor/main.py
@@ -1,14 +1,36 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
-from src.predictor.config_file import generate_config, set_config
+import argparse
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+
+
+
+def handle_args():
+    """
+    Handles arguments that are given by the user
+    :return:  args  namespace
+    """
+    parser = argparse.ArgumentParser(description='Deep fake model')
+    parser.add_argument('-device', type=str, choices=['cpu', 'cuda:0'], default='cuda:0', help='Choose cpu or gpu')
+    parser.add_argument('--load_to_ram', action='store_true', help='if flagged, loads to ram')
+    parser.add_argument('-batch_size', type=int, default=8, help='batch size')
+    return parser.parse_args()
+
+
+def main():
+    """
+    Runs a model. For more information refer to README
+    :return:
+    """
+    args = handle_args()
+
+    print("Running model with:")
+    print("device:", args.device)
+    print("Load  ram?", args.load_to_ram)
+    print("Batch size:", args.batch_size)
 
 
 if __name__ == '__main__':
-    config = generate_config()
-    print("Running configurations:")
-    print("Running on device:", config.device_name)
-    print("Load all videos to ram?", config.RAM_ONLY)
-    print("Batch size:", config.batch_size)
-    config = set_config(config, )
+    main()
+
---
+++ tests/integration/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+import os
+
+
+@pytest.mark.parametrize("device,batch,load_to_ram", [
+    ("cpu", 8, True),
+    ("cpu", 8, False),
+    ("cuda:0", 8, True),
+    ("cuda:0", 8, False),
+])
+def test_model_run(device, batch, load_to_ram):
+    main_path = "~/PycharmProjects/test-examples/src/predictor/main.py"
+    command = "python " + main_path + " -device " + device + " -batch " + str(batch)
+    if load_to_ram:
+        command = command + " --load_to_ram"
+    os.system(command)
