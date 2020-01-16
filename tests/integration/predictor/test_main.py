2893570a38ad1336fd6aac33dfe00e68c002fea3
Ran Rubin <ranrubin@gmail.com>
2020-01-16 09:46:04 +0200

    x

--- tests/integration/predictor/test_main.py
+++ tests/integration/predictor/test_main.py
@@ -6,7 +6,6 @@ import os
     Our model should run in the following way:
      src/predictor/main.py -device cpu -batch 8 --load_to_ram
 """
-
 @pytest.mark.parametrize("device,batch,load_to_ram", [
     ("cpu", 8, True),
     ("cpu", 8, False),
@@ -15,7 +14,6 @@ import os
 ])
 def test_model_run(device, batch, load_to_ram):
     """
-
     :param device: cpu or number of gpu
     :param batch: batch size
     :param load_to_ram: if true will load to ram
