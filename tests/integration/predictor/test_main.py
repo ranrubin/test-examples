0765b0f95a4e6c22b10a2f0d4ef889526d3dee79
Ran Rubin <ranrubin@gmail.com>
2020-01-16 08:55:26 +0200

    x

--- tests/integration/predictor/test_main.py
+++ tests/integration/predictor/test_main.py
@@ -2,6 +2,11 @@ import pytest
 import os
 
 
+"""
+    Our model should run in the following way:
+     src/predictor/main.py -device cpu -batch 8 --load_to_ram
+"""
+
 @pytest.mark.parametrize("device,batch,load_to_ram", [
     ("cpu", 8, True),
     ("cpu", 8, False),
@@ -16,7 +21,7 @@ def test_model_run(device, batch, load_to_ram):
     :param load_to_ram: if true will load to ram
     :return:
     """
-    main_path = "/src/predictor/main.py"
+    main_path = "/home/rrubin/PycharmProjects/test-examples/src/predictor/main.py"
     command = "python " + main_path + " -device " + device + " -batch " + str(batch)
     if load_to_ram:
         command = command + " --load_to_ram"
