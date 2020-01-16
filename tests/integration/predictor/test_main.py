4c7bcc5b5b981452824985d80e7a05c27810ba19
Ran Rubin <ranrubin@gmail.com>
2020-01-16 08:52:19 +0200

    x

--- tests/integration/predictor/test_main.py
+++ tests/integration/predictor/test_main.py
@@ -9,6 +9,13 @@ import os
     ("cuda:0", 8, False),
 ])
 def test_model_run(device, batch, load_to_ram):
+    """
+
+    :param device: cpu or number of gpu
+    :param batch: batch size
+    :param load_to_ram: if true will load to ram
+    :return:
+    """
     main_path = "/src/predictor/main.py"
     command = "python " + main_path + " -device " + device + " -batch " + str(batch)
     if load_to_ram:
