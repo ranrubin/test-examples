1e012307129bbc2960ae1f919679884b0811bcf7
Ran Rubin <ranrubin@gmail.com>
2020-01-16 08:20:29 +0200

    x

--- tests/unit/test_wallet.py
+++ tests/unit/test_wallet.py
@@ -1,3 +1,4 @@
+# tests/unit/test_wallet.py
 import pytest
 from src.wallet import Wallet, InsufficientAmount
 
@@ -9,17 +10,21 @@ from src.wallet import Wallet, InsufficientAmount
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
+    """
+    Testing that in case of logical error, an informative exception would be raised
+    :return:
+    """
     wallet = Wallet()
     with pytest.raises(InsufficientAmount):
         wallet.spend_cash(100)
 
 
-# def test_setting_wrong_type_raises_exception_on_type():
-#     """
-#         Asserts exception is raised when wrong input type is given
-#     """
-#     with pytest.raises(TypeError):
-#         wallet = Wallet("not int")
+def test_setting_wrong_type_raises_exception_on_type():
+    """
+        Asserts exception is raised when wrong input type is given
+    """
+    with pytest.raises(TypeError):
+        wallet = Wallet("not int")
 
 
 def test_default_initial_amount():
